55e3ff13ad5915247b96ab72737f525dcea37f7d
aklaaaaa <144097906+ayklaaa@users.noreply.github.com>
2024-01-19 04:31:33 +0900

    Add files via upload

---
+++ prr/ghf/qq.js
@@ -0,0 +1,254 @@
+import styles from "qq.css";
+
+const Desktop8 = () => {
+  return (
+    <div className={styles.desktop8}>
+      <img className={styles.image25Icon} alt="" src="/image-25@2x.png" />
+      <div className={styles.desktop8Child} />
+      <div className={styles.desktop8Item} />
+      <div className={styles.desktop8Inner} />
+      <div className={styles.rectangleDiv} />
+      <div className={styles.desktop8Child1} />
+      <div className={styles.desktop8Child2} />
+      <div className={styles.desktop8Child3} />
+      <div className={styles.desktop8Child4} />
+      <div className={styles.desktop8Child5} />
+      <div className={styles.desktop8Child6} />
+      <div className={styles.desktop8Child7} />
+      <div className={styles.desktop8Child8} />
+      <div className={styles.desktop8Child9} />
+      <div className={styles.desktop8Child10} />
+      <div className={styles.desktop8Child11} />
+      <div className={styles.desktop8Child12} />
+      <img className={styles.unionIcon} alt="" />
+      <div className={styles.desktop8Child13} />
+      <div className={styles.desktop8Child14} />
+      <div className={styles.desktop8Inner} />
+      <div className={styles.desktop8Child16} />
+      <div className={styles.desktop8Child17} />
+      <div className={styles.desktop8Child18} />
+      <div className={styles.desktop8Child19} />
+      <div className={styles.desktop8Child20} />
+      <div className={styles.desktop8Child21} />
+      <div className={styles.desktop8Child22} />
+      <div className={styles.desktop8Child23} />
+      <div className={styles.desktop8Child24} />
+      <div className={styles.desktop8Child25} />
+      <div className={styles.groupDiv}>
+        <div className={styles.groupChild} />
+      </div>
+      <div className={styles.desktop8Inner1}>
+        <div className={styles.groupChild} />
+      </div>
+      <div className={styles.desktop8Inner2}>
+        <div className={styles.groupChild} />
+      </div>
+      <div className={styles.desktop8Inner3}>
+        <div className={styles.groupChild} />
+      </div>
+      <div className={styles.desktop8Child26} />
+      <div className={styles.div}>Цифровое путешествие в мир слов</div>
+      <div className={styles.div1}>Онлайн-библиотека</div>
+      <div className={styles.div2}>Погрузиться в мир книг</div>
+      <img className={styles.unionIcon1} alt="" src="/union@2x.png" />
+      <img className={styles.icon} alt="" src="/-1@2x.png" />
+      <div className={styles.desktop8Inner4}>
+        <div className={styles.groupChild2} />
+      </div>
+      <div className={styles.desktop8Inner5}>
+        <div className={styles.groupChild2} />
+      </div>
+      <div className={styles.desktop8Inner6}>
+        <div className={styles.groupChild2} />
+      </div>
+      <div className={styles.desktop8Child27} />
+      <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+      <div className={styles.desktop8Child28} />
+      <img className={styles.vectorIcon1} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon2} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon3} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon4} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon5} alt="" src="/vector.svg" />
+      <div className={styles.vectorParent}>
+        <img className={styles.vectorIcon6} alt="" src="/vector.svg" />
+        <div className={styles.groupChild5} />
+      </div>
+      <div className={styles.div3}>{`Зарегистрироваться `}</div>
+      <div className={styles.div4}>Вход</div>
+      <div className={styles.div5}>Категории</div>
+      <img className={styles.vectorIcon7} alt="" src="/vector.svg" />
+      <b className={styles.b}>детям</b>
+      <b className={styles.b1}>романы</b>
+      <b className={styles.b2}>история</b>
+      <b className={styles.b3}>хорроры</b>
+      <b className={styles.b4}>комедия</b>
+      <img className={styles.vectorIcon8} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon9} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon10} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon11} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon12} alt="" src="/vector.svg" />
+      <img className={styles.groupIcon} alt="" src="/group-65.svg" />
+      <img className={styles.image36Icon} alt="" src="/image-36@2x.png" />
+      <b className={styles.b5}>Рекомендуем</b>
+      <div className={styles.div6}>Читать</div>
+      <b className={styles.b6}>Лучшие бизнес книги</b>
+      <b className={styles.b7}>Лучшие новые книги</b>
+      <b className={styles.b8}>{`Детям и родителям `}</b>
+      <b className={styles.b9}>
+        Переговоры за минуту. Экспресс-курс делового общения
+      </b>
+      <b className={styles.b10}>
+        Переговоры за минуту. Экспресс-курс делового общения
+      </b>
+      <b className={styles.b11}>
+        Переговоры за минуту. Экспресс-курс делового общения
+      </b>
+      <b className={styles.b12}>Эдуард Трымбовецкий</b>
+      <b className={styles.b13}>Кэтрин Стокетт</b>
+      <b className={styles.b14}>Кэтрин Стокетт</b>
+      <b className={styles.b15}>Кэтрин Стокетт</b>
+      <b className={styles.b16}>Кэтрин Стокетт</b>
+      <b className={styles.b17}>Эдуард Трымбовецкий</b>
+      <b className={styles.b18}>Эдуард Трымбовецкий</b>
+      <img className={styles.image37Icon} alt="" src="/image-37@2x.png" />
+      <img className={styles.image38Icon} alt="" src="/image-37@2x.png" />
+      <img className={styles.image39Icon} alt="" src="/image-37@2x.png" />
+      <div className={styles.desktop8Inner7}>
+        <div className={styles.groupChild2} />
+      </div>
+      <div className={styles.desktop8Inner8}>
+        <div className={styles.groupChild2} />
+      </div>
+      <div className={styles.desktop8Inner9}>
+        <div className={styles.groupChild2} />
+      </div>
+      <b className={styles.b19}>Григорий без отчества Бабочкин</b>
+      <b className={styles.b20}>Григорий без отчества Бабочкин</b>
+      <b className={styles.b21}>Григорий без отчества Бабочкин</b>
+      <b className={styles.b22}>Анна Зенькова</b>
+      <b className={styles.b23}>Анна Зенькова</b>
+      <b className={styles.b24}>Анна Зенькова</b>
+      <img className={styles.image43Icon} alt="" src="/image-43@2x.png" />
+      <img className={styles.image44Icon} alt="" src="/image-43@2x.png" />
+      <img className={styles.image45Icon} alt="" src="/image-43@2x.png" />
+      <div className={styles.desktop8Inner10}>
+        <div className={styles.groupChild2} />
+      </div>
+      <div className={styles.desktop8Inner11}>
+        <div className={styles.groupChild2} />
+      </div>
+      <div className={styles.rectangleParent}>
+        <div className={styles.groupChild11} />
+        <div className={styles.groupChild2} />
+      </div>
+      <b className={styles.b25}>Уснувшие дети</b>
+      <b className={styles.b26}>Прислуга</b>
+      <b className={styles.b27}>Прислуга</b>
+      <b className={styles.b28}>Прислуга</b>
+      <b className={styles.b29}>Прислуга</b>
+      <b className={styles.b30}>Уснувшие дети</b>
+      <b className={styles.b31}>Уснувшие дети</b>
+      <b className={styles.b32}>Энтони Пассерон</b>
+      <b className={styles.b33}>Энтони Пассерон</b>
+      <b className={styles.b34}>Энтони Пассерон</b>
+      <img className={styles.image40Icon} alt="" src="/image-37@2x.png" />
+      <img className={styles.image40Icon} alt="" src="/image-46@2x.png" />
+      <img className={styles.image41Icon} alt="" src="/image-37@2x.png" />
+      <img className={styles.image41Icon} alt="" src="/image-46@2x.png" />
+      <img className={styles.image42Icon} alt="" src="/image-37@2x.png" />
+      <img className={styles.image42Icon} alt="" src="/image-46@2x.png" />
+      <img className={styles.vectorIcon13} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon14} alt="" src="/vector.svg" />
+      <div className={styles.desktop8Child29} />
+      <img className={styles.vectorIcon15} alt="" src="/vector.svg" />
+      <div className={styles.desktop8Child30} />
+      <div className={styles.desktop8Child31} />
+      <img className={styles.vectorIcon16} alt="" src="/vector.svg" />
+      <div className={styles.desktop8Child32} />
+      <div className={styles.desktop8Child33} />
+      <img className={styles.vectorIcon17} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon18} alt="" src="/vector.svg" />
+      <div className={styles.desktop8Child34} />
+      <b className={styles.b35}>Читать</b>
+      <div className={styles.desktop8Child35} />
+      <div className={styles.desktop8Child36} />
+      <div className={styles.desktop8Child37} />
+      <div className={styles.desktop8Child38} />
+      <b className={styles.b36}>Читать</b>
+      <b className={styles.b37}>Читать</b>
+      <b className={styles.b38}>Читать</b>
+      <b className={styles.b39}>Читать</b>
+      <b className={styles.b40}>Читать</b>
+      <b className={styles.b41}>Читать</b>
+      <b className={styles.b42}>Читать</b>
+      <b className={styles.b43}>Читать</b>
+      <b className={styles.b44}>Читать</b>
+      <b className={styles.b45}>Читать</b>
+      <b className={styles.b46}>Читать</b>
+      <b className={styles.b47}>Читать</b>
+      <img
+        className={styles.rectangleIcon}
+        alt=""
+        src="/rectangle-215@2x.png"
+      />
+      <img
+        className={styles.desktop8Child39}
+        alt=""
+        src="/rectangle-215@2x.png"
+      />
+      <img
+        className={styles.desktop8Child40}
+        alt=""
+        src="/rectangle-215@2x.png"
+      />
+      <img
+        className={styles.desktop8Child41}
+        alt=""
+        src="/rectangle-215@2x.png"
+      />
+      <div className={styles.parent}>
+        <div className={styles.div7}>
+          <p className={styles.p}>О НАС</p>
+          <p className={styles.p}>&nbsp;</p>
+          <p className={styles.p}>Блог</p>
+          <p className={styles.p}>Поиск магазинов</p>
+          <p className={styles.p}>Карьера</p>
+          <p className={styles.p}>&nbsp;</p>
+        </div>
+        <div className={styles.div8}>
+          <p className={styles.p}>ПОМОЩЬ</p>
+          <p className={styles.p}>&nbsp;</p>
+          <p className={styles.p}>Доставка</p>
+          <p className={styles.p}>Политика возврата</p>
+          <p className={styles.p}>Связаться с нами</p>
+          <p className={styles.p}>Часто задаваемые вопросы</p>
+          <p className={styles.p}>Условия использования</p>
+        </div>
+        <div className={styles.div9}>
+          <p className={styles.p}>ЗАРЕГИСТРИРОВАТЬСЯ!</p>
+          <p className={styles.p}>&nbsp;</p>
+          <p className={styles.p}>{`Подпишитесь, чтобы первыми узнавать `}</p>
+          <p className={styles.p}>о наших новейших акциях, эксклюзивных</p>
+          <p className={styles.p}>акциях и многом другом!</p>
+        </div>
+        <div className={styles.groupChild13} />
+        <div className={styles.groupChild14} />
+        <b className={styles.b48}>Зарегистрироваться</b>
+        <div className={styles.email}>Email</div>
+      </div>
+      <div className={styles.desktop8Child42} />
+      <img
+        className={styles.notificationIcon}
+        alt=""
+        src="/39notification.svg"
+      />
+      <img className={styles.vectorIcon19} alt="" src="/vector.svg" />
+      <img className={styles.vectorIcon20} alt="" src="/vector.svg" />
+      <div className={styles.elibrarian}>eLibrarian</div>
+      <div className={styles.desktop8Child43} />
+      <b className={styles.b49}>премиум-подписка</b>
+    </div>
+  );
+};
+
+export default Desktop8;
